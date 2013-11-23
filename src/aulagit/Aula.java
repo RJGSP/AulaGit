89a9c80b4fd0dc2fa1fe1390dd8aa3065eaf46d7
rjgsp <rjgsp@iscte.pt>
2013-11-23 15:47:31 +0000

    Signed-off-by: rjgsp <rjgsp@iscte.pt>

---
+++ src/aulagit/Aula.java
@@ -0,0 +1,14 @@
+package aulagit;
+
+public class Aula {
+
+	public Aula() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
